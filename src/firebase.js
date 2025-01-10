ca1fff9b56b67038953b778a53e126053900b480
eryka <saltedpopcorn48@gmail.com>
2025-01-11 00:57:11 +0700

    change firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyB_RGbYv9rfUYWah3vDsXccpUOvYpbjsQw",
+    authDomain: "bookcafeservice.firebaseapp.com",
+    projectId: "bookcafeservice",
+    storageBucket: "bookcafeservice.firebasestorage.app",
+    messagingSenderId: "1076341342932",
+    appId: "1:1076341342932:web:1d2cc5af46bacf52247141",
+    measurementId: "G-R22KDTRKJD"
 };
 
 // Initialize Firebase
